d329fda398022c5e273c3ab80c59b9f23c32fd18
Adebimpe John E. Omolola <lamideadebimpe@gmail.com>
2025-08-13 03:16:59 +0100

    Update my_africanneurohealth.py

--- my_africanneurohealth.py
+++ my_africanneurohealth.py
@@ -137,7 +137,7 @@ def login():
 
     if st.button("Login"):
         try:
-            response = supabase.auth.sign_in_with_password({"email": email, "password": password})
+            response = supabase.auth.sign_in_with_password({"Email": email, "Password": password})
             if response.user:
                 st.session_state.user = response.user
                 st.success(f"Logged in as {email}")
@@ -1273,6 +1273,7 @@ if app_mode == "Alzheimer Risk Prediction":
         except Exception as e:
 
                 st.error(f"Error during alzheimers prediction or saving: {e}")
+
 
 
 
